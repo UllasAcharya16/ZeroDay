32999d4808a199b0b27dbc57854f977fe37b96e5
Ullas Acharya <Ullas.22cs174@sode-edu.in>
2025-12-05 01:36:29 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ClientPage from "./ClientPage.jsx";
+import LandingPage from "./LandingPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/client" element={<ClientPage />} />
+        <Route path="/app" element={<App />} />
+        {/* fallback route */}
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
